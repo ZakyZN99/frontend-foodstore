0d416af6c5c18380a4e2ec9cad2ffa836e0ce779
Zaky Zamni <zakyzn1999@gmail.com>
2024-04-09 19:52:42 +0800

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,22 @@
+import { IoPersonCircleOutline, IoCart } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div className="justify-between items-center h-18 max-w-[1540px] mx-auto bg-blue-400 text-white">
-      <div>
-          <img alt="FoodStore" src=""/>
-          
+    <div className="flex  justify-between items-center h-19 max-w-[1540px] mx-auto bg-blue-400 text-white">
+      <div className="flex items-center gap-4">
+        <img alt="FoodStore" src="" />
+        <h1>Food</h1>
+        <h1>Drink</h1>
+        <h1>Snack</h1>
       </div>
-      {/* 
-      <DropdownButton id="dropdown-basic-button" title="Categories">
-        <Dropdown.Item href="#/action-1">Food</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Drink</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Snack</Dropdown.Item>
-      </DropdownButton>
-
-      <div>
-        <input></input>
-        <button>Search</button>
+      <div className="flex justify-between items-center gap-3" >
+        <div>
+          <input></input>
+          <button>Search</button>
+        </div>
+        <IoCart size={30}/>
+        <IoPersonCircleOutline size={30}/>
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-8 h-8"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-        />
-      </svg>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-8 h-8"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-        />
-      </svg> */}
     </div>
   );
 };
